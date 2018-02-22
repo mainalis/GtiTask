a6327c18800f4914c0fa1765494732ae82448f8c
sushil Mainali <sushilmainali@MacBook-Pro.local>
2018-02-22 07:24:08 +0000

    controller page

--- src/main/java/com/gti/spring/controller/welcomePageController.java
+++ src/main/java/com/gti/spring/controller/welcomePageController.java
@@ -3,6 +3,7 @@ package com.gti.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by sushilmainali on 21/02/2018.
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class welcomePageController {
+public class WelcomePageController {
 
+    @RequestMapping(method = RequestMethod.GET)
     public String home(Model map)
     {
         return "index";
